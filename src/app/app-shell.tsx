9a53e3aa7dfa0de09b34833e2d7c04bfa2754e17
SERVICIO DE FIESTAS SALTO AK PRODUCCIONES EVENTOS <akproduccionessalto@gmail.com>
2025-08-29 21:11:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button className=

--- src/app/app-shell.tsx
+++ src/app/app-shell.tsx
@@ -291,7 +291,7 @@ export function AppShell({ children }: { children: ReactNode }) {
       <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
         {children}
       </main>
-      {pathname === '/' && <AkAssistant />}
+      
     </div>
   );
 }
