72edc209c9069d402e1a376f9bac6ebab329fcb4
SERVICIO DE FIESTAS SALTO AK PRODUCCIONES EVENTOS <akproduccionessalto@gmail.com>
2025-10-04 22:47:37 +0000

    Try fixing this error: `Runtime Error: Error: Link is not defined. Error

--- src/components/gastronomia/GestionReposteria.tsx
+++ src/components/gastronomia/GestionReposteria.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import React, { useState, useEffect, useMemo, type FormEvent } from 'react';
+import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
