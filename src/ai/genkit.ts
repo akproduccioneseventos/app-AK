96490e0cba7dff47433e960e8e766943e02c8430
SERVICIO DE FIESTAS SALTO AK PRODUCCIONES EVENTOS <akproduccionessalto@gmail.com>
2025-10-04 22:33:22 +0000

    no funciona (_for element <Primitive.li>_)

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,10 @@
 
 import 'dotenv/config';
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
   // This model will be used by default in all flows unless overridden.
-  model: 'gemini-1.5-flash', 
+  model: 'gemini-pro', 
 });
