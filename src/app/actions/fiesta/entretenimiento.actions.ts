c5eb32f17a5a423af7847a793fa952a75aea022a
akproduccioneseventos <akproduccionessalto@gmail.com>
2026-05-11 09:59:49 -0300

    Fix forbidden module variable name

--- src/app/actions/fiesta/entretenimiento.actions.ts
+++ src/app/actions/fiesta/entretenimiento.actions.ts
@@ -113,9 +113,9 @@ export async function uploadEntretenimientoMedia(formData: FormData) {
 
     const current = getStoredEntertainment(fiesta) || {};
     const modules = { ...(current.modules || {}) };
-    const module = { ...(modules[moduleId] || {}) };
-    module.media = [mediaItem, ...(module.media || [])];
-    modules[moduleId] = module;
+    const entertainmentModule = { ...(modules[moduleId] || {}) };
+    entertainmentModule.media = [mediaItem, ...(entertainmentModule.media || [])];
+    modules[moduleId] = entertainmentModule;
 
     const nextEntertainment = normalizeEntertainmentData({
       ...current,
